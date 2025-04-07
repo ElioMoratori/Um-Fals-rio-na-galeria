55c960f3f0eccb146a5f70e9126f28d0d6d2e2fb
ElioMoratori <moratori.elio@gmail.com>
2025-04-07 12:46:49 -0300

    feat -alterações em tela com funções exibindo palavra escolhida

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ pegaPalavras();
 const pegaTítulo = document.querySelector('h1')
 const pegaTexto = document.querySelector('p')
 const botao = document.querySelector('button')
+const divSeletores = document.querySelector('.select-box')
 
 let listaPalavras
 let pegaJogadores = document.querySelector('select').value
 let palavraEscolhida
+let quantidadeJogadores
 
 
 async function pegaPalavras() {
-    const request = await fetch('./palavras.json')
-    const listaJson = await request.json()
-    return listaPalavras = await listaJson
-    
+        try {
+            const request = await fetch('./palavras.json')
+            const listaJson = await request.json()
+            return listaPalavras = await listaJson
+        } 
+        
+        catch {
+            alert('Não foi possível buscar as palavras')
+            throw error
+        }
 }
 
 
@@ -23,6 +31,25 @@ function selecionaPalavra() {
     palavraEscolhida = palavras[numero]
 }
 
+function exibePalavraEscolhida() {
+    pegaTítulo.textContent = palavraEscolhida.palavra
+    pegaTexto.innerHTML = `A dica é <b>${palavraEscolhida.dica}</b>`
+    botao.textContent = 'Próximo Jogador'
+    botao.onclick = proximoJogador
+}
 
+function proximoJogador() {
+    pegaTítulo.textContent = "Jogador nº"
+    pegaTexto.innerHTML = "Clique quando estiver pronto"
+    botao.textContent = 'Exibir a palavra'
+    botao.onclick = exibePalavraEscolhida
+}
 
+function jogar() {
+    let quantidadeJogadores = document.querySelector('select').value
+    divSeletores.classList.remove('select-box')
+    divSeletores.classList.add('disabled')
+    selecionaPalavra()
+    proximoJogador()
+}
 
